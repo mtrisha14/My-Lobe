53594688cbb29c43f86bb478346284927c8681d2
mtrisha14 <mtrisha1407@gmail.com>
2026-03-31 03:26:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const gifStages = [
     "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
+    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",     
+    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",
+    "https://media1.tenor.com/m/rOiSuwloJpsAAAAd/emrananhret.gif",
+    "https://media1.tenor.com/m/9fscv__vkukAAAAd/dudu-bubu-dudu-bubu-love.gif",
+    "https://media.tenor.com/li30kFEG50UAAAAi/bubu-bubu-dudu.gif",// 2 pleading
     "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
     "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
     "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
